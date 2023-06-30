74bc65c8177bb53575546dcdc87d753c8e810d9b
mrdizlike <billchiper12@gmail.com>
2023-06-30 16:49:40 +0300

    Updated FirebaseAPI, added signIn, signOut and fetching user

---
+++ LocaViva/MainAppViews/ProfileView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileView.swift
+//  LocaViva
+//
+//  Created by Виктор on 30.06.2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewMode
+    
+    var body: some View {
+        if let user = viewModel.currentUser {
+            List {
+                Section {
+                    Text(user.firstName)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
